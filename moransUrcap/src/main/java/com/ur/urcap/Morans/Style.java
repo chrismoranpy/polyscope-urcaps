f3646923b2052356092841422d73ce7d3b931aae
ncstarr <chrismoran.py@outlook.com>
2021-10-15 07:22:12 -0700

    Add files via upload
    latest changes after work friday urcap will compile and load into ursim but gives error saying its using polyscopes internal packages which is not allowed

---
+++ moransUrcap/src/main/java/com/ur/urcap/Morans/Style.java
@@ -0,0 +1,13 @@
+package com.ur.urcap.Morans;
+
+import java.awt.Dimension;
+
+public abstract class Style {
+
+	public abstract int getVerticalSpacing();
+
+	public abstract int getHorizontalIndent();
+
+	public abstract Dimension getButtonSize();
+
+}
